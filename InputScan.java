9d0412b252fc9709fa781989b6fc7d9267dd05fe
Kerfufl <c19382426@mytudublin.ie>
2021-03-18 19:06:10 +0000

    Implemented line skip on file read

--- InputScan.java
+++ InputScan.java
@@ -2,26 +2,40 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 
+//Class to read MLdata file, with functions pertaining to such
 public class InputScan
 {
     private String file;
+    private String[] factors;
     
-
+    //Only need to instantiate the file name
     public InputScan(String file)
     {
         this.file = file;
     }
-
+    //Function to debug file reading 
     public void Printline()
     {
         try
         {
             File csv = new File(file);
             Scanner scan = new Scanner(csv);
+            int i=0;
+            for(i=0;i<10;i++)
+            {
+                if(i>3)
+                {
+                    System.out.println(scan.nextLine());
+                } else {
+                    scan.nextLine();
+                }
+            }
 
-            System.out.println(scan.nextLine());
-            scan.close();
+            //System.out.println(scan.nextLine());
             
+            scan.close();
+
+
         } catch(FileNotFoundException e)
         {
             System.out.println("File not found!");
