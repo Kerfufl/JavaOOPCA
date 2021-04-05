96624df692afc3aa4b0dd30abd26385e3242c8a4
Kerfufl <c19382426@mytudublin.ie>
2021-04-05 20:52:05 +0100

    Added class for Bayes calculations

---
+++ Bayes.java
@@ -0,0 +1,10 @@
+public class Bayes {
+    private int total;
+    private float[][] percent;
+
+    public Bayes(int total, float[][] percent)
+    {
+        this.total = total;
+        this.percent = percent;
+    }
+}
