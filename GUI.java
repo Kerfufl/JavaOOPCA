b4837eb83e535aac35154ec706e817862b0b298c
Kerfufl <c19382426@mytudublin.ie>
2021-04-07 22:44:40 +0100

    Testing dropdown menus for inputs

--- GUI.java
+++ GUI.java
@@ -1,5 +1,5 @@
 //import java.awt.Color;
-//import java.awt.FlowLayout;
+import java.awt.FlowLayout;
 import java.awt.BorderLayout;
 //import java.awt.event.ActionEvent;
 //import java.awt.event.ActionListener;
@@ -9,6 +9,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JComboBox;
 //import javax.swing.JButton;
 
 //Class to generate gui, which will facilitate input/ output of data
@@ -17,6 +18,7 @@ public class GUI extends JFrame{
     JPanel panel;
     JTable table;
     JScrollPane pane;
+    JComboBox drop;
 
     static final long serialVersionUID = 0;
     public GUI(String title, InputScan i)
@@ -26,15 +28,17 @@ public class GUI extends JFrame{
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
 
-        panel = new JPanel(new BorderLayout());
-
+        panel = new JPanel(new FlowLayout());
+        
         table = new JTable(i.getHold(),i.getTitle());
         
+        drop = new JComboBox(new String[] {"bruh","unbruh"});
         //table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
 
         
         pane = new JScrollPane(table);
-        panel.add(pane, BorderLayout.NORTH);
+        //panel.add(pane, BorderLayout.NORTH);
+        panel.add(drop, BorderLayout.NORTH);
 
         add(panel, BorderLayout.NORTH);
 
